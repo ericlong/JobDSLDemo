4c39c74f44c61604cbef576594236cfa7e27e58b
Eric Long <github@ericlong.com>
2016-06-07 14:29:11 -0500

    Update JobDSLDemo.groovy

--- JobDSLDemo.groovy
+++ JobDSLDemo.groovy
@@ -18,7 +18,7 @@ def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 branches.each {
     def branchName = it.name
-    def jobName = "${project}-${branchName}".replaceAll('-')
+    def jobName = "${project}-${branchName}"
     job(jobName) {
         scm {
             git("git://github.com/${project}.git", branchName)
