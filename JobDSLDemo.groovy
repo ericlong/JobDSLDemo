67c023c38aa7c051de93c6e5425ef34c5a38d646
Eric Long <github@ericlong.com>
2016-06-08 10:33:45 -0500

    Update JobDSLDemo.groovy

--- JobDSLDemo.groovy
+++ JobDSLDemo.groovy
@@ -1,4 +1,4 @@
-/*
+
 
 folder('FreestyleJob')
 folder('quidryan')
@@ -15,6 +15,7 @@ job('FreestyleJob/DSL-Tutorial-1-Test') {
     }
 }
 
+/*
 def project = 'quidryan/aws-sdk-test'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
